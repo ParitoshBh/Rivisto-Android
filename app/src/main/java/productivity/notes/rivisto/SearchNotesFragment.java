99d1ab830fb146dc5a7b3ea64648efe5f9961ba6
ParitoshBh <paritosh@techvorm.com>
2016-10-25 18:59:19 -0400

    Revamped search activity to include labels and search results.

--- app/src/main/java/productivity/notes/rivisto/SearchNotesFragment.java
+++ app/src/main/java/productivity/notes/rivisto/SearchNotesFragment.java
@@ -8,20 +8,37 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SearchNotesFragment extends Fragment {
-    private RecyclerView recyclerView;
+import java.util.ArrayList;
+
+import productivity.notes.rivisto.utils.Helpers;
+
+public class SearchNotesFragment extends Fragment implements TextView.OnEditorActionListener {
+    private EditText noteSearchQuery;
+    private RecyclerView recyclerView, recyclerViewSearch;
     private FirebaseRecyclerAdapter<Tag, TagHolder> adapter;
-    private DatabaseReference firebaseRef;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private DatabaseReference firebaseRef, firebaseNotesRef;
+    private ArrayList<Note> notes;
+    private static final String LOG_SEARCH = "SearchEvent";
 
     public SearchNotesFragment() {
     }
@@ -32,6 +49,8 @@ public class SearchNotesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search_notes, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerViewSearch = (RecyclerView) view.findViewById(R.id.recycler_view_search_results);
+        noteSearchQuery = (EditText) getActivity().findViewById(R.id.noteSearchQuery);
 
         SharedPreferences sharedPref = getActivity().getSharedPreferences("FirebaseCredentials", Context.MODE_PRIVATE);
         String userKey = sharedPref.getString(getString(R.string.userKey), null);
@@ -40,11 +59,20 @@ public class SearchNotesFragment extends Fragment {
             FirebaseApp firebaseApp = FirebaseApp.getInstance("Firebase");
             FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance(firebaseApp);
             firebaseRef = firebaseDatabase.getReference("/tags");
+            firebaseNotesRef = firebaseDatabase.getReference("/notes");
         } else {
-            firebaseRef = FirebaseDatabase.getInstance().getReference(userKey + "/tags");
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            firebaseRef = firebaseDatabase.getReference(userKey + "/tags");
+            firebaseNotesRef = firebaseDatabase.getReference(userKey + "/notes");
         }
 
         new getTags().execute();
+
+        noteSearchQuery.setOnEditorActionListener(this);
+
+        recyclerViewSearch.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewSearch.setHasFixedSize(true);
+        recyclerViewSearch.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
@@ -79,6 +107,73 @@ public class SearchNotesFragment extends Fragment {
         @Override
         protected void onPostExecute(Boolean result) {
             recyclerView.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+        if (i == EditorInfo.IME_ACTION_SEARCH) {
+            performSearch();
+        }
+
+        return false;
+    }
+
+    private void performSearch() {
+        Helpers.hideKeyboard(getActivity(), getView());
+        String searchQuery = noteSearchQuery.getText().toString().trim().toLowerCase();
+        //Log.i(LOG_SEARCH, searchQuery);
+        new searchNotes().execute(searchQuery);
+    }
+
+    private class searchNotes extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            notes = new ArrayList<>();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... query) {
+
+            final String searchQuery = query[0];
+
+            firebaseNotesRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Note note = dataSnapshot.getValue(Note.class);
+                    if (note.getTitle().toLowerCase().contains(searchQuery) || note.getContent().toLowerCase().contains(searchQuery)) {
+                        notes.add(note);
+                    }
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            recyclerViewSearch.setAdapter(new RecyclerViewAdapter(notes));
         }
     }
 }
