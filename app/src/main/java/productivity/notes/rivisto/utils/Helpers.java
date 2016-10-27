f70c92bce580b98b17300dba3bf3f250a5e5274b
ParitoshBh <paritosh@techvorm.com>
2016-10-26 22:10:08 -0400

    Added helper class to check internet connectivity status.

--- app/src/main/java/productivity/notes/rivisto/utils/Helpers.java
+++ app/src/main/java/productivity/notes/rivisto/utils/Helpers.java
@@ -2,6 +2,8 @@ package productivity.notes.rivisto.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -10,5 +12,16 @@ public class Helpers {
     public static void hideKeyboard(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static boolean isConnectedToInternet(Context context){
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
     }
 }
